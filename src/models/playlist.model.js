b4bda2ffb75197ed2d3a69cac91960f5d29aafda
Sahil MJ <sahilmj07@gmail.com>
2026-06-02 00:15:33 +0530

    feat: add models for comments, likes, playlists, and tweets with necessary schemas

---
+++ src/models/playlist.model.js
@@ -0,0 +1,13 @@
+const { default: mongoose } = require("mongoose");
+
+const playlistSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: "true", trim: true, index: true },
+    description: { type: String },
+    videos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Video" }],
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true },
+);
+
+export const Playlist = mongoose.model("Playlist", playlistSchema);
